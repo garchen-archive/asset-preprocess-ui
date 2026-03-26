577bd31fa01c159ac09e35fba0b858f769ce0db7
Ubin Malla <ubin.malla@gmail.com>
2026-03-26 07:36:58 +0545

    ci: test

--- middleware.ts
+++ middleware.ts
@@ -5,11 +5,12 @@ export const config = {
     /*
      * Match all request paths except for the ones starting with:
      * - api/auth (NextAuth API routes)
+     * - api/health (health check endpoint)
      * - login (login page)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    "/((?!api/auth|login|_next/static|_next/image|favicon.ico).*)",
+    "/((?!api/auth|api/health|login|_next/static|_next/image|favicon.ico).*)",
   ],
 };
